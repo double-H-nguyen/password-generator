68c4943bde112c5e09404bb53532561ecbd95cb5
Henry Nguyen <nguyenhenryh1996@gmail.com>
2021-06-06 16:53:39 -0500

    Version 1
    Exploring one way to generate a random password (https://www.youtube.com/watch?v=xnX9KbBwLCw)

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+from random import randint
+
+
 def main():
-    print('test')
+    password = uppercase_only()
+    # password = upper_and_lowercase()
+    print(f"Here is your password: {password}")
+
+
+def uppercase_only():
+    # all upper case password
+    password = ""
+    for i in range(10):
+        i = chr(randint(65, 90))
+        password = str(password) + i
+    return password
+
+
+def upper_and_lowercase():
+    # upper and lowercase password
+    password = ""
+    for i in range(5):
+        i = chr(randint(65, 90))
+        j = chr(randint(65, 90)).lower()
+        password = str(password) + i + j
+    return password
+
 
 if __name__ == "__main__":
     main()
